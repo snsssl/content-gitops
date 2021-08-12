710c63a8cde85bc5d6ed2a95d98d55372ceca953
snsssl <lakshmanburugupalli@gmail.com>
2021-08-12 15:17:49 +0530

    Update app.py

--- python/app.py
+++ python/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello la student La!"
+    return "Hello la student La 521!"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000)
